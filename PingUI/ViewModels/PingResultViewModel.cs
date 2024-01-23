40aa1187f8202b7bd05589f44202b2f809e124b3
Richard Robertson <richard@rarobertson.net>
2024-01-23 13:22:30 -0600

    Localize ping results
    Closes #10

--- PingUI/ViewModels/PingResultViewModel.cs
+++ PingUI/ViewModels/PingResultViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net.NetworkInformation;
+using PingUI.I18N;
 using PingUI.Models;
 
 namespace PingUI.ViewModels;
@@ -29,6 +30,43 @@ public class PingResultViewModel : ViewModelBase
 	public IPStatus Status
 	{
 		get;
+	}
+
+	public string LocalizedStatus
+	{
+		get
+		{
+			return Status switch
+			{
+				IPStatus.Success => Strings.System_Net_NetworkInformation_IPStatus_Success,
+				IPStatus.DestinationNetworkUnreachable => Strings.System_Net_NetworkInformation_IPStatus_DestinationNetworkUnreachable,
+				IPStatus.DestinationHostUnreachable => Strings.System_Net_NetworkInformation_IPStatus_DestinationHostUnreachable,
+				// DestinationProhibited and DestinationProtocolUnreachable have the same value
+				// DestinationProtocolUnreachable is for IPv4 and DestinationProhibited is for IPv6
+				// IPStatus.DestinationProhibited => Strings.System_Net_NetworkInformation_IPStatus_DestinationProhibited,
+				IPStatus.DestinationProtocolUnreachable => Strings.System_Net_NetworkInformation_IPStatus_DestinationProtocolUnreachable,
+				IPStatus.DestinationPortUnreachable => Strings.System_Net_NetworkInformation_IPStatus_DestinationPortUnreachable,
+				IPStatus.NoResources => Strings.System_Net_NetworkInformation_IPStatus_NoResources,
+				IPStatus.BadOption => Strings.System_Net_NetworkInformation_IPStatus_BadOption,
+				IPStatus.HardwareError => Strings.System_Net_NetworkInformation_IPStatus_HardwareError,
+				IPStatus.PacketTooBig => Strings.System_Net_NetworkInformation_IPStatus_PacketTooBig,
+				IPStatus.TimedOut => Strings.System_Net_NetworkInformation_IPStatus_TimedOut,
+				IPStatus.BadRoute => Strings.System_Net_NetworkInformation_IPStatus_BadRoute,
+				IPStatus.TtlExpired => Strings.System_Net_NetworkInformation_IPStatus_TtlExpired,
+				IPStatus.TtlReassemblyTimeExceeded => Strings.System_Net_NetworkInformation_IPStatus_TtlReassemblyTimeExceeded,
+				IPStatus.ParameterProblem => Strings.System_Net_NetworkInformation_IPStatus_ParameterProblem,
+				IPStatus.SourceQuench => Strings.System_Net_NetworkInformation_IPStatus_SourceQuench,
+				IPStatus.BadDestination => Strings.System_Net_NetworkInformation_IPStatus_BadDestination,
+				IPStatus.DestinationUnreachable => Strings.System_Net_NetworkInformation_IPStatus_DestinationUnreachable,
+				IPStatus.TimeExceeded => Strings.System_Net_NetworkInformation_IPStatus_TimeExceeded,
+				IPStatus.BadHeader => Strings.System_Net_NetworkInformation_IPStatus_BadHeader,
+				IPStatus.UnrecognizedNextHeader => Strings.System_Net_NetworkInformation_IPStatus_UnrecognizedNextHeader,
+				IPStatus.IcmpError => Strings.System_Net_NetworkInformation_IPStatus_IcmpError,
+				IPStatus.DestinationScopeMismatch => Strings.System_Net_NetworkInformation_IPStatus_DestinationScopeMismatch,
+				IPStatus.Unknown => Strings.System_Net_NetworkInformation_IPStatus_Unknown,
+				_ => Strings.System_Net_NetworkInformation_IPStatus_Unknown,
+			};
+		}
 	}
 
 	/// <summary>
