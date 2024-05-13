b12c89adb194a6f31811598638011178a743b0de
Richard Robertson <richard@rarobertson.net>
2024-05-13 12:56:21 -0500

    Add `Tags` property to `Target`

--- PingUI/Models/Target.cs
+++ PingUI/Models/Target.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Net;
 using System.Text.Json.Serialization;
 using PingUI.JsonConverters;
@@ -11,14 +13,20 @@ namespace PingUI.Models;
 public record Target
 {
 	/// <summary>
+	/// Backing store for <see cref="GetHashCode" />.
+	/// </summary>
+	private readonly Lazy<int> hashCode;
+
+	/// <summary>
 	/// Initializes a new <see cref="Target" /> with the specified properties.
 	/// </summary>
 	/// <param name="address">The <see cref="IPAddress" /> to ping.</param>
 	/// <param name="label">An optional human readable label.</param>
 	/// <param name="coolDown">The time to wait between pings.</param>
+	/// <param name="tags">A set of <see cref="string" /> tags or <see langword="null" />.</param>
 	/// <exception cref="ArgumentNullException"><paramref name="address" /> is <see langword="null" /></exception>
 	/// <exception cref="ArgumentOutOfRangeException"><paramref name="coolDown" /> is less than <see cref="TimeSpan.Zero" /> or greater than one day.</exception>
-	public Target(IPAddress address, string? label, TimeSpan coolDown)
+	public Target(IPAddress address, string? label, TimeSpan coolDown, ImmutableSortedSet<string>? tags)
 	{
 		ArgumentNullException.ThrowIfNull(address);
 		ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(coolDown, TimeSpan.Zero);
@@ -26,6 +34,19 @@ public record Target
 		Address = address;
 		Label = label;
 		CoolDown = coolDown;
+		Tags = tags ?? ImmutableSortedSet<string>.Empty;
+		hashCode = new Lazy<int>(() =>
+		{
+			var hash = new HashCode();
+			hash.Add(Address);
+			hash.Add(Label);
+			hash.Add(CoolDown);
+			foreach (var tag in Tags)
+			{
+				hash.Add(tag);
+			}
+			return hash.ToHashCode();
+		});
 	}
 
 	/// <summary>
@@ -36,7 +57,6 @@ public record Target
 	public IPAddress Address
 	{
 		get;
-		init;
 	}
 
 	/// <summary>
@@ -46,7 +66,6 @@ public record Target
 	public string? Label
 	{
 		get;
-		init;
 	}
 
 	/// <summary>
@@ -56,6 +75,31 @@ public record Target
 	public TimeSpan CoolDown
 	{
 		get;
-		init;
+	}
+
+	/// <summary>
+	/// Gets the collection of tags on this <see cref="Target" />.
+	/// </summary>
+	/// <value>An <see cref="ImmutableSortedSet{T}" /> of <see cref="string" />.</value>
+	public ImmutableSortedSet<string> Tags
+	{
+		get;
+	}
+
+	/// <inheritdoc />
+	public virtual bool Equals(Target? other)
+	{
+		return other is not null
+			&& hashCode.Value == other.hashCode.Value
+			&& Address == other.Address
+			&& Label == other.Label
+			&& CoolDown == other.CoolDown
+			&& Tags.SetEquals(other.Tags);
+	}
+
+	/// <inheritdoc />
+	public override int GetHashCode()
+	{
+		return hashCode.Value;
 	}
 }
